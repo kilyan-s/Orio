f3b3bae34df836b4236586b7a8ca9d089ba51f4f
Sockalingum Kilyan <sockalingum.kilyan@gmail.com>
2015-01-15 23:41:05 +0100

    Jeu 3
    Changement des frequences de EQ, fourchette de déplacement de chaque EQ
    et validation
    Reste: nombre de vies à définir

--- Jeu/jeu3.js
+++ Jeu/jeu3.js
@@ -73,7 +73,7 @@ jeu3.prototype.init = function() {
   // Choisir le type du filtre, ici c'est peaking, ce type de filtre permet de sélectionner une fréquence bien spécifique 
 	filtre86.type = "peaking";
 	// On choisit la frequence du filtre
-	filtre86.frequency.value = 86;
+	filtre86.frequency.value = 76;
 	// On choisit "l'étendue" du filtre, si ce chiffre est grand on va bien cibler la fréquence, si celui-ci est bas on vas sélectionner plus de fréquence aux alentours
 	filtre86.Q.value = 20;
 	// Permet de monter le gain ou non de cette fréquence --> va de -40 à 40
@@ -81,19 +81,19 @@ jeu3.prototype.init = function() {
 
 	filtre132 = context.createBiquadFilter();
 	filtre132.type = "peaking";
-	filtre132.frequency.value = 132;
+	filtre132.frequency.value = 122;
 	filtre132.Q.value = 20;
 	filtre132.gain.value = 0;
              
 	filtre175 = context.createBiquadFilter();
 	filtre175.type = "peaking";
-	filtre175.frequency.value = 175;
+	filtre175.frequency.value = 153;
 	filtre175.Q.value = 20;
 	filtre175.gain.value = 0; 
 	
 	filtre220 = context.createBiquadFilter();
 	filtre220.type = "peaking";
-	filtre220.frequency.value = 220;
+	filtre220.frequency.value = 200;
 	filtre220.Q.value = 20;
 	filtre220.gain.value = 0;  
 	
@@ -134,21 +134,35 @@ jeu3.prototype.init = function() {
 
 *******/
 	function testGoupille() {
-		if (crochetPos >= (goupille1 - 1) && crochetPos <= (goupille1 + 1)){
+		if (filtre86.frequency.value >= (goupille1 - 1) && filtre86.frequency.value <= (goupille1 + 1)){
 			console.log("Goupille 1");
-			goupille1bool = true;
+			goupille1bool = true; 
 			//On passe au 2e EQ
+			//On passe le gain de l'EQ 86Hz à 0
+			filtre86.gain.value = 0;
+			//On change la frequence du filtre lowpass
+			filtrelow.frequency.value = 150;
+			//On passe le gain de l'EQ 132Hz à 11
+			filtre132.gain.value = 11;
 
 		}
-		else if (crochetPos >= (goupille2 - 1) && crochetPos <= (goupille2 + 1)){
+		if (filtre132.frequency.value >= (goupille2 - 1) && filtre132.frequency.value <= (goupille2 + 1)){
 			console.log("Goupille 2");
 			goupille2bool = true;
+			//On passe à l'EQ 3
+			filtre132.gain.value = 0;
+			filtrelow.frequency.value = 200;
+			filtre175.gain.value = 8;
 		}
-		else if(crochetPos >= (goupille3 - 2) && crochetPos <= (goupille3 + 3)){
+		if(filtre175.frequency.value >= (goupille3 - 2) && filtre175.frequency.value <= (goupille3 + 3)){
 			console.log("Goupille 3");
 			goupille3bool = true;
+			//On passe a l'EQ 4
+			filtre175.gain.value = 0;
+			filtrelow.frequency.value = 260;
+			filtre220.gain.value = 8;
 		}
-		else if(crochetPos >= (goupille4 - 1) && crochetPos <= (goupille4 + 4)){
+		if(filtre220.frequency.value >= (goupille4 - 1) && filtre220.frequency.value <= (goupille4 + 4)){
 			console.log("Goupille 4");
 			goupille4bool = true;
 		}
@@ -188,26 +202,92 @@ function finJeuWin(){
 				if(goupille1bool == false && goupille2bool == false && goupille3bool == false && goupille4bool == false){
 					// Deplacer EQ1
 					console.log("Deplacer EQ1");
+					//ON increment la fréquence du filtre
+					filtre86.frequency.value += 1;
+					console.log(filtre86.frequency.value);
+					//Le max de la frequence du filtre est 96Hz
+					if (filtre86.frequency.value >= 96){
+						filtre86.frequency.value = 96; 
+						console.log("Vous ne pouvez plus avancer");
+					}
 				}
 				else if(goupille1bool == true && goupille2bool == false && goupille3bool == false && goupille4bool == false){
 					//Deplacer EQ2
 					console.log("Deplacer EQ2");
+					filtre132.frequency.value += 1;
+					console.log(filtre132.frequency.value);
+					if (filtre132.frequency.value >= 142){
+						filtre132.frequency.value = 142; 
+						console.log("Vous ne pouvez plus avancer");
+					}
 				}
 				else if (goupille1bool == true && goupille2bool == true && goupille3bool == false && goupille4bool == false){
 					//Deplacer EQ3
 					console.log("Deplacer EQ3");
+					filtre175.frequency.value += 1;
+					console.log(filtre175.frequency.value);
+					if (filtre175.frequency.value >= 198){
+						filtre175.frequency.value = 198; 
+						console.log("Vous ne pouvez plus avancer");
+					}
+				}
+				else if (goupille1bool == true && goupille2bool == true && goupille3bool == true && goupille4bool == false){
+					// Deplacer EQ4
+					console.log("Deplacer EQ4"); 
+					filtre220.frequency.value += 1;
+					console.log(filtre220.frequency.value);
+					if (filtre220.frequency.value >= 250){
+						filtre220.frequency.value = 250; 
+						console.log("Vous ne pouvez plus avancer");
+					}
+				}
+				// crochetPos += deplacement;
+				// console.log(crochetPos);
+				break;
+			//Reculer
+			case "F":
+				if(goupille1bool == false && goupille2bool == false && goupille3bool == false && goupille4bool == false){
+					// Deplacer EQ1
+					console.log("Deplacer EQ1");
+					filtre86.frequency.value -= 1;
+					console.log(filtre86.frequency.value);
+					if (filtre86.frequency.value <= 76){
+						filtre86.frequency.value = 76; 
+						console.log("Vous ne pouvez plus reculer");
+					}
+				}
+				else if(goupille1bool == true && goupille2bool == false && goupille3bool == false && goupille4bool == false){
+					//Deplacer EQ2
+					console.log("Deplacer EQ2");
+					filtre132.frequency.value -= 1;
+					console.log(filtre132.frequency.value);
+					if (filtre132.frequency.value <= 122){
+						filtre132.frequency.value = 122; 
+						console.log("Vous ne pouvez plus reculer");
+					}
+				}
+				else if (goupille1bool == true && goupille2bool == true && goupille3bool == false && goupille4bool == false){
+					//Deplacer EQ3
+					console.log("Deplacer EQ3");
+					filtre175.frequency.value -= 1;
+					console.log(filtre175.frequency.value);
+					if (filtre175.frequency.value <= 153){
+						filtre175.frequency.value = 153; 
+						console.log("Vous ne pouvez plus reculer");
+					}
 				}
 				else if (goupille1bool == true && goupille2bool == true && goupille3bool == true && goupille4bool == false){
 					// Deplacer EQ4
 					console.log("Deplacer EQ4");
+					filtre220.frequency.value -= 1;
+					console.log(filtre220.frequency.value);
+					if (filtre220.frequency.value <= 200){
+						filtre220.frequency.value = 200; 
+						console.log("Vous ne pouvez plus reculer");
+					}
 				}
-				crochetPos += deplacement;
-				console.log(crochetPos);
-				break;
-			//Reculer
-			case "F":
-				crochetPos -= deplacement;
-				console.log(crochetPos);
+				// crochetPos -= deplacement;
+				// console.log(crochetPos);
 				break;
 			//Lever
 			case " ":
